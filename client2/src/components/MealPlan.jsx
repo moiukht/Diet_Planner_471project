9ff1c74c92c07e51112a51293e698620b08f29f3
Tahsina <moiukh29@gmail.com>
2024-04-19 13:49:38 +0600

    mealplanbackground

--- client2/src/components/MealPlan.jsx
+++ client2/src/components/MealPlan.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import OpenAI from "openai";
 import axios from 'axios';
+import dietbg from './dietbg.webp'
 // import { PromptTemplate } from 'langchain/prompts';
 // import { ResponseSchema, StructuredOutputParser } from 'langchain/output_parsers';
 
@@ -126,11 +127,19 @@ const MealPlan = () => {
   };
 
   return (
-<div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-beige-100 to-beige-300">
-  <div className="max-w-4xl w-full mx-auto flex mt-20">
-    {/* Form Section */}
+<div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-beige-100 to-beige-300 relative">
+    {/* Background Image */}
+    <div className="absolute inset-0 z-0">
+      <img
+        src={dietbg}  
+        alt="Background"
+        className="object-cover w-full h-full opacity-65"
+      />
+    </div>
 
-    <div className="w-1/2 p-4">
+    <div className="max-w-4xl w-full mx-auto flex mt-20 relative z-10"> {/* Ensure z-index is higher than the background */}
+      {/* Form Section */}
+      <div className="w-1/2 p-4">
     <label htmlFor="dietary" className="mb-1 block text-xl text-gray-800">Required Calories</label>
       <input
         className="mb-2 p-2 border rounded-md w-full"
